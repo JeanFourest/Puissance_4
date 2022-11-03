cdda61588669ce20599f008a599a3ed896fc8be2
Jean Fourest <jmfourest@gmail.com>
2022-11-03 21:28:29 +0100

    Update dataBase.sql

---
+++ dataBase.sql
@@ -0,0 +1,28 @@
+/*STORY 2*/
+INSERT INTO utilisateur (email, password, name, dateSignUp)
+VALUES ("jm@gmail.com", "ABCD", "PapyBruh", "2022-10-10");
+
+
+
+
+/*STORY 3-1*/
+UPDATE utilisateur
+SET password = "new password"
+WHERE id = 1;
+
+/*STORY 3-2*/
+UPDATE utilisateur
+SET email = "jm2@gmail.com"
+WHERE id = 1 AND password = "new password";
+
+
+
+
+/*STORY 4*/
+SELECT * FROM utilisateur WHERE email = "jm2@gmail.com" AND password = "new password";
+
+
+
+/*STORY 5*/
+INSERT INTO jeu (gameName)
+VALUES ("The Power Of Memory")
