4d14a30b814d8d929039ef47f2ed780442e8857f
Jean Fourest <jean.fourest@edu.esiee-it.fr>
2022-11-04 14:55:31 +0100

    Update dataBase.sql

--- dataBase.sql
+++ dataBase.sql
@@ -1,11 +1,11 @@
-/*STORY 2*/
+/*STORY 2 creation de compte*/
 INSERT INTO utilisateur (email, password, name, dateSignUp)
 VALUES ("jm@gmail.com", "ABCD", "PapyBruh", "2022-10-10");
 
 
 
 
-/*STORY 3*/
+/*STORY 3 modifier le mot de passe et le mail*/
 
 UPDATE utilisateur
 SET password = "new password"
@@ -18,18 +18,18 @@ WHERE id = 1 AND password = "new password";
 
 
 
-/*STORY 4*/
+/*STORY 4 recherche un utilisateur*/
 SELECT * FROM utilisateur WHERE email = "jm2@gmail.com" AND password = "new password";
 
 
 
-/*STORY 5*/
+/*STORY 5 creation de jeu*/
 INSERT INTO jeu (gameName)
 VALUES ("The Power Of Memory");
 
 
 
-/*STORY 6*/
+/*STORY 6 afficher les scores de mes utilisateurs*/
 SELECT gameName, name, difficulty, gameScore
 FROM jeu INNER JOIN score INNER JOIN utilisateur
 ON score.idGame = jeu.id AND score.idPlayer = utilisateur.id
@@ -38,24 +38,27 @@ ORDER BY jeu.gameName, score.difficulty, score.gameScore;
 
 
 
-/*STORY 7  */
+/*STORY 7 afficher les scores de mes utilisateurs avec des filtres*/
 SELECT gameName, name, difficulty, gameScore
 FROM jeu INNER JOIN score INNER JOIN utilisateur
 ON score.idGame = jeu.id AND score.idPlayer = utilisateur.id
 ORDER BY jeu.gameName; /*ou on peut mettre score.difficulty; ou score.gameScore;*/
 
 
-/*STORY 8*/
+/*STORY 8 si il y a un doublon, il faudra le supprimer*/
+SELECT idGame, COUNT(idGame), difficulty, COUNT(difficulty)
+FROM score
+GROUP BY idGame, difficulty
+HAVING COUNT(idGame) > 1 AND COUNT(difficulty) >1;
 
 
-
-/*STORY 9*/
+/*STORY 9 envoyer un message sur le chat général de mon site*/
 INSERT INTO message (idGame,idExpediteur,message)
 VALUES (1,1,"just testing")
 
 
 
-/*STORY 10*/
+/*STORY 10 afficher la discussion du chat général PAS COMPLETEMENT FINI*/
 SELECT message.message,message.idExpediteur, message.dateMessage
 FROM message
 WHERE message.dateMessage >= NOW() - INTERVAL 1 DAY;
