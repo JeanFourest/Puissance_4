4abf7554b065631b85c5bc733322d5b41458f61c
Jean Fourest <jmfourest@gmail.com>
2022-11-10 14:10:44 +0100

    Update database.inc.php

--- logindocs/database.inc.php
+++ logindocs/database.inc.php
@@ -7,9 +7,12 @@ $password = "root";
 $dbname = "memoryGame";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+//$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
+//if ($conn->connect_error) {
+//  die("Connection failed: " . $conn->connect_error);
+//}
+
+$conn = new PDO("mysql:host=$servername:8889;dbname);
+
 ?>
