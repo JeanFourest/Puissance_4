97bd079f7c8c5b918868dbe81ac3c63a05b34bcc
Jean Fourest <jmfourest@gmail.com>
2022-11-11 14:44:52 +0100

    FINITO

--- includes/database.inc.php
+++ includes/database.inc.php
@@ -2,14 +2,18 @@
 //on utilisera ce fichier pour include toute les autres page
 //On se connecte a la base de donnee
 $servername = "localhost";
+$dbname = "memoryGame";
 $username = "root";
 $password = "root";
-$dbname = "memoryGame";
 
-// cree connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+
+try{
+$conn = new PDO("mysql:host=$servername:8889;dbname=$dbname", $username, $password);
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+}
+catch(PDOException $e){
+  echo "connexion erreur: " . $e->getMessage();
 }
 ?>
