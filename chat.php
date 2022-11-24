fd68ba60f9d1e141866c4209b230cbd99d56725e
Creepynax <116552630+Creepynax@users.noreply.github.com>
2022-11-24 13:32:22 +0100

    Chat.php finish
    Chat.php terminé et fonctionnel

--- chat.php
+++ chat.php
@@ -28,69 +28,65 @@ $id_user = $_SESSION['user_id'];
         <div class="main">
             <table id="tableau">
 
-    <?php
-        if(isset($_POST['valider'])){
-            if(!empty($_POST['message'])){
-                $message = nl2br(htmlspecialchars($_POST['message']));
+        <?php
+            if(isset($_POST['valider'])){
+                if(!empty($_POST['message'])){
+                    $message = nl2br(htmlspecialchars($_POST['message']));
 
-                $insererMessage = $conn -> prepare('INSERT INTO message(idGame, idExpediteur, message) VALUES (1, ?, ?)');
-                $insererMessage -> bindParam(1, $id_user);
-                $insererMessage -> bindParam(2, $message);
-                $insererMessage -> execute(); 
+                    $insererMessage = $conn -> prepare('INSERT INTO message(idGame, idExpediteur, message) VALUES (1, ?, ?)');
+                    $insererMessage -> bindParam(1, $id_user);
+                    $insererMessage -> bindParam(2, $message);
+                    $insererMessage -> execute(); 
+                }
+                else{
+                    echo "Veuillez écrire un message avant de vouloir l'envoyer !";
+                }
             }
-            else{
-                echo "Veuillez écrire un message avant de vouloir l'envoyer !";
+
+            $respons3 = $conn -> query('SELECT * FROM message INNER JOIN utilisateur ON message.idExpediteur = utilisateur.id ORDER BY message.dateMessage');
+
+            while($discution3 = $respons3 -> fetch()){
+                if ($discution3['idExpediteur'] == $id_user) {
+                ?>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td id="nameUser" colspan="2">Moi</td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td class="messageEnvoye" colspan="2"><?= $discution3['message']; ?></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td id="date" colspan="2"><?= $discution3['dateMessage']; ?></td>
+                    </tr>
+                <?php
+                }
+                else{
+                ?>
+                    <tr>
+                        <td></td>
+                        <td id="nameUser" colspan="2"><?= $discution3['name']; ?></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td><img src="assets/Images/pdpHywel.jpeg" alt="Photo de profil utilisateur" class="pdpUser"></td>
+                        <td class="messageRecu" colspan="2"><?= $discution3['message']; ?></td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td></td>
+                        <td id="date" colspan="2"><?= $discution3['dateMessage']; ?></td>
+                        <td></td>
+                    </tr>
+            <?php
+                }
             }
-        }
-
-        $respons = $conn -> prepare('SELECT * FROM message INNER JOIN utilisateur ON message.idExpediteur = utilisateur.id WHERE utilisateur.id = ?');
-        $respons -> bindParam(1, $id_user);
-        $respons -> execute();
-        $discution = $respons -> fetch();
-
-        $respons2 = $conn -> prepare('SELECT * FROM message INNER JOIN utilisateur ON message.idExpediteur = utilisateur.id WHERE utilisateur.id != ?');
-        $respons2 -> bindParam(1, $id_user);
-        $respons2 -> execute();
-        $discution2 = $respons2 -> fetch();
-
-        $respons3 = $conn -> query('SELECT * FROM message INNER JOIN utilisateur ON message.idExpediteur = utilisateur.id');
-
-        while($discution3 = $respons3 -> fetch()){
-    ?>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td id="nameUser">Moi</td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td class="messageEnvoye"><?= $discution['message']; ?></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td></td>
-                    <td id="date"><?= $discution['dateMessage']; ?></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td id="nameUser"><?= $discution2['name']; ?></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td><img src="assets/Images/pdpHywel.jpeg" alt="Photo de profil utilisateur" class="pdpUser"></td>
-                    <td class="messageRecu"><?= $discution2['message']; ?></td>
-                    <td></td>
-                </tr>
-                <tr>
-                    <td></td>
-                    <td id="date"><?= $discution2['dateMessage']; ?></td>
-                    <td></td>
-                </tr>
-    <?php
-        }
-        $respons3 -> closeCursor();
-    ?>
+            $respons3 -> closeCursor();
+            ?>
             </table>
         </div>
 
